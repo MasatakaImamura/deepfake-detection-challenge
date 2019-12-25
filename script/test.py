87c916542cf7693a412c0e4ef19c1bfb511f7af6
MasatakaImamura <masataka.i.navi@gmail.com>
2019-12-25 23:52:14 +0900

    no message

--- script/test.py
+++ script/test.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 
-from utils.model_init import model_init, NormalCnn
+from utils.model_init import model_init
 from utils.data_augumentation import ImageTransform
 from utils.utils import seed_everything, get_metadata, get_mov_path
 from utils.dfdc_dataset import DeepfakeDataset_idx0, DeepfakeDataset_continuous
@@ -17,10 +17,13 @@ data_dir = '../input'
 seed = 0
 img_size = 224
 batch_size = 1
-epoch = 6
-model_name = 'test'
+epoch = 8
+model_name = 'efficientnet-b0'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 criterion = nn.BCEWithLogitsLoss()
+img_num = 5
+frame_window = 20
+
 
 # Set Seed
 seed_everything(seed)
@@ -34,8 +37,13 @@ mov_path = get_mov_path(metadata, data_dir, fake_per_real=1)
 train_mov_path, val_mov_path = train_test_split(mov_path, test_size=0.1, random_state=seed)
 
 # Dataset
-train_dataset = DeepfakeDataset_continuous(train_mov_path, metadata, transform=ImageTransform(img_size), phase='train')
-val_dataset = DeepfakeDataset_continuous(val_mov_path, metadata, transform=ImageTransform(img_size), phase='val')
+train_dataset = DeepfakeDataset_continuous(
+    train_mov_path, metadata, transform=ImageTransform(img_size),
+    phase='train', img_num=img_num, frame_window=frame_window)
+
+val_dataset = DeepfakeDataset_continuous(
+    val_mov_path, metadata, transform=ImageTransform(img_size),
+    phase='val', img_num=img_num, frame_window=frame_window)
 
 # DataLoader
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -49,14 +57,28 @@ print('DataLoader Already')
 
 # Model  ################################################################
 torch.cuda.empty_cache()
-net = NormalCnn()
+net = model_init(model_name)
 
-optimizer = optim.SGD(params=net.parameters(), lr=0.001, momentum=0.9)
+# Transfer Learning  ################################################################
+# Specify The Layers for updating
+params_to_update = []
+update_params_name = ['_fc.weight', '_fc.bias']
+
+for name, param in net.named_parameters():
+    if name in update_params_name:
+        param.requires_grad = True
+        params_to_update.append(param)
+    else:
+        param.requires_grad = False
+
+optimizer = optim.RMSprop(params=params_to_update, lr=0.256,
+                          alpha=0.99, eps=1e-08, weight_decay=0.9, momentum=0.9, centered=False)
 print('Model Already')
 
 # Train  ################################################################
-net, best_loss = train_model(net, dataloader_dict, criterion, optimizer, num_epoch=epoch, device=device)
+net, best_loss = train_model(net, dataloader_dict, criterion, optimizer,
+                             num_epoch=epoch, device=device, model_name=model_name)
 
 # Save Model  ################################################################
 date = datetime.datetime.now().strftime('%Y%m%d')
-torch.save(net.state_dict(), "../model/{}_acc{:.3f}_{}.pth".format(model_name, best_loss, date))
+torch.save(net.state_dict(), "../model/{}_continue_acc{:.3f}_{}.pth".format(model_name, best_loss, date))
