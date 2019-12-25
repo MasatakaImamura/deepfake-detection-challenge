df40482a0cf5e1b079f3a1e00aa459dc2ea07ffd
今村匡貴 <>
2019-12-25 12:49:58 +0900

    no message

--- script/train_model.py
+++ script/train_model.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from utils.model_init import model_init
 from utils.data_augumentation import ImageTransform
 from utils.utils import seed_everything, get_metadata, get_mov_path
-from utils.dfdc_dataset import DeepfakeDataset_idx0
+from utils.dfdc_dataset import DeepfakeDataset_idx0, DeepfakeDataset_continuous
 from utils.trainer import train_model
 
 
@@ -18,7 +18,7 @@ seed = 0
 img_size = 224
 batch_size = 4
 epoch = 6
-model_name = 'efficientnet-b1'
+model_name = 'resnet50'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 criterion = nn.BCEWithLogitsLoss()
 
@@ -34,8 +34,8 @@ mov_path = get_mov_path(metadata, data_dir, fake_per_real=1)
 train_mov_path, val_mov_path = train_test_split(mov_path, test_size=0.1, random_state=seed)
 
 # Dataset
-train_dataset = DeepfakeDataset_idx0(train_mov_path, metadata, transform=ImageTransform(img_size), phase='train')
-val_dataset = DeepfakeDataset_idx0(val_mov_path, metadata, transform=ImageTransform(img_size), phase='val')
+train_dataset = DeepfakeDataset_continuous(train_mov_path, metadata, transform=ImageTransform(img_size), phase='train')
+val_dataset = DeepfakeDataset_continuous(val_mov_path, metadata, transform=ImageTransform(img_size), phase='val')
 
 # DataLoader
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -54,7 +54,7 @@ net = model_init(model_name)
 # Transfer Learning  ################################################################
 # Specify The Layers for updating
 params_to_update = []
-update_params_name = ['_fc.weight', '_fc.bias']
+update_params_name = ['fc.weight', 'fc.bias']
 
 for name, param in net.named_parameters():
     if name in update_params_name:
