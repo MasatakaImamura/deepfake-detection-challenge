058dff0ca18bcf1b034966eb8e956b5bc8048b0e
MasatakaImamura <masataka.i.navi@gmail.com>
2020-01-09 08:08:55 +0900

    no message

--- script/utils/Conv3D.py
+++ script/utils/Conv3D.py
@@ -8,46 +8,20 @@ class Conv3dnet(nn.Module):
 
         self.block_3d_1 = nn.Sequential(
             nn.Conv3d(in_channels=3, out_channels=64, kernel_size=5, stride=2),
+            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=5, stride=2),
             nn.BatchNorm3d(64),
             nn.ReLU(inplace=True)
         )
-        self.pool_1 = nn.MaxPool3d(2)
+        self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.dropout = nn.Dropout(0.5)
 
-        self.block_3d_2 = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
-            nn.BatchNorm3d(64),
-            nn.ReLU(inplace=True)
-        )
-        self.pool_2 = nn.MaxPool3d(2)
-
-        self.block_3d_3 = nn.Sequential(
-            nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
-            nn.BatchNorm3d(128),
-            nn.ReLU(inplace=True)
-        )
-        self.pool_3 = nn.MaxPool3d(2)
-
-        self.block_2d_1 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=5, stride=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
-        )
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        self.fc = nn.Linear(256, output_size)
+        self.fc = nn.Linear(64, output_size)
 
     def forward(self, x):
         x = self.block_3d_1(x)
-        x = self.pool_1(x)
-        x = self.block_3d_2(x)
-        x = self.pool_2(x)
-        x = self.block_3d_3(x)
-        x = self.pool_3(x)
-        x = x.squeeze()
-        x = self.block_2d_1(x)
         x = self.avgpool(x)
         x = x.squeeze()
+        x = self.dropout(x)
         x = self.fc(x)
 
         return x
