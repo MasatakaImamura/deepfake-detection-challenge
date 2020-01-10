e988eb6c7803f6035c48f41ac2f06fbe24c9ac3e
今村匡貴 <>
2020-01-10 16:47:17 +0900

    residentを追加

--- script/utils/Conv3D.py
+++ script/utils/Conv3D.py
@@ -6,22 +6,43 @@ class Conv3dnet(nn.Module):
     def __init__(self, output_size=2):
         super(Conv3dnet, self).__init__()
 
+        self.conv3d_down = nn.Conv3d(in_channels=3, out_channels=64, kernel_size=5, stride=2, padding=1)
+
         self.block_3d_1 = nn.Sequential(
-            nn.Conv3d(in_channels=3, out_channels=64, kernel_size=5, stride=2),
-            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=5, stride=2),
+            nn.Conv3d(in_channels=3, out_channels=64, kernel_size=5, stride=2, padding=1),
             nn.BatchNorm3d(64),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=False),
+            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=False)
         )
+
+        self.block_3d_2 = nn.Sequential(
+            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=False),
+            nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=False)
+        )
+
         self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
         self.dropout = nn.Dropout(0.5)
-
         self.fc = nn.Linear(64, output_size)
 
     def forward(self, x):
-        x = self.block_3d_1(x)
-        x = self.avgpool(x)
-        x = x.squeeze()
-        x = self.dropout(x)
-        x = self.fc(x)
 
-        return x
+        residual = self.conv3d_down(x)
+        out = self.block_3d_1(x)
+
+        out += residual
+        residual2 = out
+        out = self.block_3d_2(out)
+        out += residual2
+
+        out = self.avgpool(out)
+        out = out.squeeze()
+        out = self.dropout(out)
+        out = self.fc(out)
+
+        return out
